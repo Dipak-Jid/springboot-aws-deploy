aa77ca01b9209cc1c98b859dbec81e533464f7d9
Dipak-Jid <73751682+Dipak-Jid@users.noreply.github.com>
2025-05-28 14:32:16 +0000

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "Message from AWS ECS - V13";
+        return "Message from AWS ECS - V14";
     }
 
     @GetMapping("/message")
